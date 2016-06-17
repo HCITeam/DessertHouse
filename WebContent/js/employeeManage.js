fd2c782b129f41f49f84a89d99ea4c605ace9d2c
xiaoskeleton <xiaoskeleton@DESKTOP-B3D3EDH>
2016-06-17 17:24:59 +0800

    22233

--- WebContent/js/employeeManage.js
+++ WebContent/js/employeeManage.js
@@ -1,18 +1,84 @@
 /**
  * 管理服务员界面
  */
-$(document).on("click",".store-btn-edit",function(){
+$(document).on("click",".store-btn-edit",function()
+{
 	var name=$(this).attr("id").split("-")[0];
-	$("#edit-name").html(name);
-    $("#employee-edit").show();
-	$("body").css("overflow","hide");
+	var idId="#"+name+"-id";
+	var typeId="#"+name+"-type";
+	var passwordId="#"+name+"-password";
+	if($(this).attr("disabled")=="disabled") return;
+	if($(passwordId).attr("disabled")=="disabled")
+	{
+		$(idId).html("读取中");
+		//$(idId).html("<select disabled='disabled' id='"+name+"-editId'><option value='0'>读取中</option></select>");
+		$(typeId).html("<select id='"+name+"-editType'><option value='2'>总店服务员</option><option value='3'>分店服务员</option></select>");
+		$(passwordId).removeAttr("disabled");
+		$(passwordId).addClass("showBorder");
+		$("#"+name+"-edit").attr("disabled","disabled");
+		$.ajax({
+	        type:"GET",
+	        url:"/Desserthouse/api/ListStore",
+	        success:function(result,textStatus){
+	        	if($(passwordId).attr("disabled")=="disabled") return;
+	        	var stores=result['store_list'];
+	        	$(idId).html("<select disabled='disabled' id='"+name+"-editId'><option value='0'>读取中</option></select>");
+	        	$("#"+name+"-editId").html("");
+	        	$.each(stores,function(index,store)
+	        	{
+	        		var id=store.id;
+	        		var nameS=store.name;
+	        		$("#"+name+"-editId").append("<option value='"+id+"'>"+nameS+"</option>");
+	        	});
+	        	
+	        	$("#"+name+"-edit").removeAttr("disabled");
+	        	$("#"+name+"-editId").removeAttr("disabled");
+	        }
+	        ,error:function(data)
+	        {
+	        	$("body").html(data.responseText);
+	        }
+	    });
+	}
+	else
+	{
+		
+		var changeId=$("#"+name+"-editId").val();
+		var changeType=$("#"+name+"-editType").val();
+		var pass=$(passwordId).val();
+		if(changeType==2)$(idId).html("总店");
+		else $(idId).html($("#"+name+"-editId").find("option:selected").text());
+		$(typeId).html($("#"+name+"-editType").find("option:selected").text());
+		$(passwordId).attr("disabled","disabled");
+		$(passwordId).removeClass("showBorder");
+		$(passwordId).val("");
+		$.ajax({
+            type:"POST",
+            url:"/Desserthouse/api/UpdateEmployee",
+            data:{'s_id':changeId,'name':name,'work_type':changeType,'password':pass},
+            success:function(result,textStatus){
+                	//alert(result.message);
+            }
+            ,error:function(data)
+            {
+            	$("body").html(data.responseText);
+            }
+        });
+		
+	}
+	
+	//$("#edit-name").html(name);
+    //$("#employee-edit").show();
+	//$("body").css("overflow","hide");
 });
 
 function addClick()
 {
-	$("#addStoreLine").html("<td><input type='text'  id='add-name' placeholder='请输入用户名'></td>\
-							<td><select id='add-s-id'><option value='0'>读取中</option></select></td>\
+	
+	$("#addStoreLine").html("<td><input type='text' class='showBorder' id='add-name' placeholder='请输入用户名'></td>\
+							<td id='add-readid'>读取中</td>\
 							<td><select id='add-type'><option value='2'>总店服务员</option><option value='3'>分店服务员</option></select></td>\
+							<td><input type='text' class='showBorder' id='add-password' placeholder='在此输入密码'></td>\
 							<td><a id='newStoreAdd'><img class='delImg'\
 							src='../img/check.png'></a></td>\
 							<td><a id='newStoreDel'><img class='delImg'\
@@ -30,6 +96,7 @@ function addClick()
         url:"/Desserthouse/api/ListStore",
         success:function(result,textStatus){
         	var stores=result['store_list'];
+        	$("#add-readid").html("<select id='add-s-id'><option value='0'>读取中</option></select>");
         	$("#add-s-id").html("");
         	$.each(stores,function(index,store)
         	{
@@ -55,12 +122,19 @@ function addEmployee()
 	var name=$("#add-name").val();
     var s_id=$("#add-s-id").val();
     var type=$("#add-type").val();
-    var passwored='admin';
-    var pass='admin';
+    var passwored=$("#add-password").val();
+    var pass=$("#add-password").val();
+
     if(name=="")
     {
     	document.getElementById('add-name').focus();
     	$("#add-name").attr("placeholder","用户名不能为空");
+    	return;
+    }
+    else if(pass=="")
+    {
+    	document.getElementById('add-password').focus();
+    	$("#add-password").attr("placeholder","密码不能为空");
     	return;
     }
     else if (s_id==""||name==""||type==""||pass==""||passwored=="") {
@@ -84,9 +158,10 @@ function addEmployee()
                 success:function(result,textStatus){
                 		$("#addStoreLine").remove();
                     	$("#store-table").append("<tr class='tableBottomTr'>"+
-                    		"<td>"+name+"</td>"+
-                    		"<td>"+result.store_name+"</td>"+
-                    		"<td>"+result.work_type+"</td>"+
+                    		"<td id='"+name+"'-name'>"+name+"</td>"+
+                    		"<td id='"+name+"'-id'>"+result.store_name+"</td>"+
+                    		"<td id='"+name+"'-type'>"+result.work_type+"</td>"+
+                    		"<td><input disabled='true' type='text'  id='"+name+"'-password' placeholder='在此输入新密码'></td>"+
                     		"<td><a class=\"store-btn-edit\" id=\""+name+"-edit\"><img src=\"../img/edit.png\"></a></td>"+
                     		"<td><a class=\"store-btn-delete\" id=\""+name+"-delete\"><img src=\"../img/delete2.png\"></a></td>"+
                     		"</tr>"+
